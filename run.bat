d4b4b02375db46425b3e629881c44f309a378047
Azazel <trunghoang1124@gmail.com>
2025-04-09 23:03:36 +0700

    Backup Succeed

--- run.bat
+++ run.bat
@@ -1,4 +1,4 @@
-java -Xmx2G -Xms2G -jar server.jar nogui
+java -Xmx2G -Xms2G -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+UseCompressedOops -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+DisableExplicitGC -Dfile.encoding=UTF-8 -Dsun.stdout.encoding=UTF-8 -Dsun.stderr.encoding=UTF-8 -jar server.jar nogui
 pause
 git add .
 git commit -m "Backup Succeed"
